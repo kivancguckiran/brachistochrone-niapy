4ccbaf619d6ff43e682074c393faee3a166fa867
kivancguckiran <kivancguckiran@gmail.com>
2018-08-17 01:10:10 +0300

    added local niapy again, tryout others...

--- optimization.py
+++ optimization.py
@@ -1,5 +1,9 @@
+import os, sys
+
+sys.path.insert(0, os.path.abspath("../NiaPy"))
+
 from util import mediums
-from NiaPy.algorithms.basic import BatAlgorithm, FireflyAlgorithm
+from NiaPy.algorithms.basic import BatAlgorithm, DifferentialEvolutionAlgorithm, ArtificialBeeColonyAlgorithm, GeneticAlgorithm, CamelAlgorithm
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,12 +26,12 @@ class Brachistochrone(object):
             return env.calculateFitness(sol)
         return evaluate
 
-# algo = BatAlgorithm(15, 100, 10000, 0.5, 0.5, 0.1, 0.9, Brachistochrone())
-# algo.best
-# algo = FireflyAlgorithm(15, 100, 10000, 0.5, 0.5, 0.5, Brachistochrone())
-# algo.Fireflies[0]
+# algo = BatAlgorithm(D=15, NP=100, nFES=10000, A=0.5, r=0.5, Qmin=0.0, Qmax=2.0, benchmark=Brachistochrone())
+# algo = DifferentialEvolutionAlgorithm(D=15, NP=100, nFES=10000, F=0.5, CR=0.9, benchmark=Brachistochrone())
+# algo = ArtificialBeeColonyAlgorithm(D=15, NP=100, nFES=10000, Limit=100, benchmark=Brachistochrone())
+# algo = GeneticAlgorithm(D=15, NP=100, nFES=10000, Ts=4, Mr=0.05, CR=0.9, benchmark=Brachistochrone())
+# algo = CamelAlgorithm(D=15, NP=100, nFES=10000, omega=0.25, mu=0.5, alpha=0.5, S_init=10, E_init=10, T_min=-1, T_max=10, benchmark=Brachistochrone())
 
-algo.run()
-
-env.drawSolution()
+solution, score = algo.run()
+env.drawSolution(solution)
 plt.show()
