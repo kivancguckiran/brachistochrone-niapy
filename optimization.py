ffc0163a4fd06c028da41828e5525e0842a4d44b
Kıvanç Güçkıran <kivancguckiran@gmail.com>
2018-08-20 14:04:27 +0300

    uptimization py updates
    put working algorithms for ni

--- optimization.py
+++ optimization.py
@@ -3,7 +3,8 @@ import os, sys
 sys.path.insert(0, os.path.abspath("../NiaPy"))
 
 from util import mediums
-from NiaPy.algorithms.basic import BatAlgorithm, DifferentialEvolutionAlgorithm, ArtificialBeeColonyAlgorithm, GeneticAlgorithm, CamelAlgorithm
+from NiaPy.algorithms.basic import BatAlgorithm, DifferentialEvolutionAlgorithm
+from NiaPy.algorithms.other import MultipleTrajectorySearch
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,9 +29,7 @@ class Brachistochrone(object):
 
 # algo = BatAlgorithm(D=15, NP=100, nFES=10000, A=0.5, r=0.5, Qmin=0.0, Qmax=2.0, benchmark=Brachistochrone())
 # algo = DifferentialEvolutionAlgorithm(D=15, NP=100, nFES=10000, F=0.5, CR=0.9, benchmark=Brachistochrone())
-# algo = ArtificialBeeColonyAlgorithm(D=15, NP=100, nFES=10000, Limit=100, benchmark=Brachistochrone())
-# algo = GeneticAlgorithm(D=15, NP=100, nFES=10000, Ts=4, Mr=0.05, CR=0.9, benchmark=Brachistochrone())
-# algo = CamelAlgorithm(D=15, NP=100, nFES=10000, omega=0.25, mu=0.5, alpha=0.5, S_init=10, E_init=10, T_min=-1, T_max=10, benchmark=Brachistochrone())
+# algo = MultipleTrajectorySearch(D=15, NP=100, nFES=10000, benchmark=Brachistochrone())
 
 solution, score = algo.run()
 env.drawSolution(solution)
