b1360b90f460e9024ea5c1f523c9187a38e5e7ad
Kıvanç Güçkıran <kivancguckiran@gmail.com>
2018-08-20 15:13:40 +0300

    optimization updated
    extended algorithms

--- optimization.py
+++ optimization.py
@@ -3,8 +3,9 @@ import os, sys
 sys.path.insert(0, os.path.abspath("../NiaPy"))
 
 from util import mediums
-from NiaPy.algorithms.basic import BatAlgorithm, DifferentialEvolutionAlgorithm
-from NiaPy.algorithms.other import MultipleTrajectorySearch
+from NiaPy.algorithms.basic import ArtificialBeeColonyAlgorithm, DifferentialEvolutionAlgorithm, EvolutionStrategy1p1, EvolutionStrategyMp1, BareBonesFireworksAlgorithm
+from NiaPy.algorithms.modified import HybridBatAlgorithm, DynNPSelfAdaptiveDifferentialEvolutionAlgorithm, SelfAdaptiveDifferentialEvolutionAlgorithm
+from NiaPy.algorithms.other MultipleTrajectorySearch
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,9 +28,15 @@ class Brachistochrone(object):
             return env.calculateFitness(sol)
         return evaluate
 
-# algo = BatAlgorithm(D=15, NP=100, nFES=10000, A=0.5, r=0.5, Qmin=0.0, Qmax=2.0, benchmark=Brachistochrone())
-# algo = DifferentialEvolutionAlgorithm(D=15, NP=100, nFES=10000, F=0.5, CR=0.9, benchmark=Brachistochrone())
-# algo = MultipleTrajectorySearch(D=15, NP=100, nFES=10000, benchmark=Brachistochrone())
+# algo = ArtificialBeeColonyAlgorithm(D=D, nGEN=nGEN, benchmark=Brachistochrone())
+# algo = DifferentialEvolutionAlgorithm(D=D, nGEN=nGEN, F=0.5, CR=0.9, benchmark=Brachistochrone())
+# algo = EvolutionStrategy1p1(D=D, nGEN=nGEN, benchmark=Brachistochrone())
+# algo = EvolutionStrategyMp1(D=D, nGEN=nGEN, benchmark=Brachistochrone())
+# algo = BareBonesFireworksAlgorithm(D=D, nGEN=nGEN, benchmark=Brachistochrone())
+# algo = HybridBatAlgorithm(D=D, nGEN=nGEN, NP=40, A=0.9, r=0.1, F=0.001, CR=0.9, Qmin=0.0, Qmax=2.0, benchmark=Brachistochrone())
+# algo = DynNPSelfAdaptiveDifferentialEvolutionAlgorithm(NP=50, D=D, nGEN=nGEN, F=0.5, F_l=-1, F_u=2.0, Tao1=0.1, CR=0.45, Tao2=0.25, benchmark=Brachistochrone())
+# algo = SelfAdaptiveDifferentialEvolutionAlgorithm(NP=50, D=D, nGEN=nGEN, F=0.5, F_l=-1, F_u=2.0, Tao1=0.1, CR=0.45, Tao2=0.25, benchmark=Brachistochrone())
+# algo = MultipleTrajectorySearch(D=D, nGEN=nGEN, benchmark=Brachistochrone())
 
 solution, score = algo.run()
 env.drawSolution(solution)
